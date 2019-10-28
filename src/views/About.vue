a21e21813e0862c82a650a613c0f81985de280f4
wahyono17 <wahyono17@gmail.com>
2019-10-28 18:03:36 +0700

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="container">
+        <input type="text" name="" id="" class="bg-gray-300 px-4 py-2" v-model="fruit" @input="filterFruits">
+        <div v-if="filteredFruits">
+            <ul>
+                <li v-for="filteredFruit in filteredFruits" v-bind:key="filteredFruit.id">{{filteredFruit}}</li>
+            </ul>
+        </div>
+   </div>
+   
 </template>
+
+<script>
+export default {
+    data:function(){
+        return{
+            fruit:'',
+            fruits:[
+                'semangka','jeruk','alpokat','kelengkeng'
+            ],
+            filteredFruits:[]
+        }
+    },
+    methods:{
+        filterFruits(){
+            this.filteredFruits = this.fruits.filter(fruit=>{
+                return fruit.toLowerCase().startsWith(this.fruit.toLowerCase());
+                });
+        }
+    }
+}
+</script>
